679692119d270154df74ef0da55f59ff42b6cadb
Kauan Muriel Rossi da Silva <kauan.silva@benner.com.br>
2023-01-17 09:42:40 -0300

    App locale, number, and date formatting

--- SalesWebMvc/Startup.cs
+++ SalesWebMvc/Startup.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Localization;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
@@ -9,6 +10,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Globalization;
 using Microsoft.EntityFrameworkCore;
 using SalesWebMvc.Models;
 using SalesWebMvc.Data;
@@ -47,9 +49,19 @@ namespace SalesWebMvc
 			services.AddScoped<DepartmentService>();
 		}
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, SeedingService seedingService)
-        {
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IHostingEnvironment env, SeedingService seedingService)
+		{
+			var enUS = new CultureInfo("en-US");
+			var localizationOption = new RequestLocalizationOptions
+			{
+				DefaultRequestCulture = new RequestCulture(enUS),
+				SupportedCultures = new List<CultureInfo> { enUS },
+				SupportedUICultures = new List<CultureInfo> { enUS }
+			};
+
+			app.UseRequestLocalization(localizationOption);
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
